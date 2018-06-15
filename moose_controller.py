214a462af062c19509e639c8551aff73c0704c67
Will Schlitzer <schlitzer90@gmail.com>
2018-06-14 21:38:13 -0500

    Initial commit

---
+++ moose_controller.py
@@ -0,0 +1,7 @@
+class MooseCreator():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.age = 0
+        self.calf_year = 0
---
+++ test_moose_populator.py
@@ -0,0 +1,19 @@
+from moose_controller import MooseCreator
+import random
+moose_num = 10
+max_x = 50
+max_y = 75
+moose_dict = {}
+moose_locs = []
+for i in range(moose_num):
+    empty_space = False
+    while empty_space == False:
+        x = random.randint(0, max_x)
+        y = random.randint(0, max_y)
+        moose_loc = (x, y)
+        if moose_loc not in moose_locs:
+            moose_locs.append(moose_loc)
+            empty_space = True
+    moose_dict[str(i)] = MooseCreator(x, y)
+
+print(moose_locs)
