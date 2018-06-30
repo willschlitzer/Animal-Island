b7fb033d96fb136d89cacd26aa3483269bd34781
Will Schlitzer <schlitzer90@gmail.com>
2018-06-30 13:47:30 -0500

    Created the IslandCreator class and the vegetation_begin() function

---
+++ island_instance.py
@@ -0,0 +1,18 @@
+class IslandCreator:
+    def __init__(self, max_x, max_y, lake_num, moose_start):
+        import random
+        self.max_x = max_x
+        self.max_y = max_y
+        self.moose_start = moose_start
+
+
+    def vegetation_begin(self):
+        veg_dict = {}
+        for x in range(max_x):
+            for y in range(max_y):
+                if random.randint(0, 100) <= 85:
+                    veg_dict[(x,y)] = [True, random.randint(10, 100)]
+                else:
+                    veg_dict[(x,y)] = [False, random.randint(0,9)]
+        self.veg_dict = veg_dict
+
