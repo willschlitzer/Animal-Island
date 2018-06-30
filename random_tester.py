1409779e65113e75cf33a0ae43ffbe7285d4a723
Will Schlitzer <schlitzer90@gmail.com>
2018-06-30 13:47:06 -0500

    Deleted several extraneous files and initial commit for island_instance.py

--- random_tester.py
+++
@@ -1,9 +0,0 @@
-import random
-
-rando_list = []
-for i in range(1000):
-    rando_list.append(random.randint(-1, 1))
-
-print('-1: ' + str(rando_list.count(-1)))
-print('0: ' + str(rando_list.count(0)))
-print('1: ' + str(rando_list.count(1)))
